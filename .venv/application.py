824b4a8a4542c9035592effac79ca22b6328255d
jherna9 <jherna1990@gmail.com>
2024-02-27 14:59:26 -0500

    changes and test running

--- .venv/application.py
+++ .venv/application.py
@@ -1,47 +1,27 @@
 from flask import Flask, request, jsonify
 import sqlite3
 import csv
-from cleaning_csv import clean_departments
+from io import TextIOWrapper
 
 app = Flask(__name__)
 
-# Function to create database table if not exists
-#departments
-def create_table_departments():
+# Function to create SQLite database table if not exists
+def create_table():
     conn = sqlite3.connect('data.db')
     c = conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS departments
-                 (id INTEGER PRIMARY KEY AUTOINCREMENT, department STRING)''')
+    c.execute('''CREATE TABLE IF NOT EXISTS csv_data
+                 (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT,age INTEGER, city TEXT)''')
     conn.commit()
     conn.close()
 
-#hired_employees
-def create_table_hired_employees():
+# Function to insert CSV data into the database
+def insert_csv_data(content):
     conn = sqlite3.connect('data.db')
     c = conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS hired_employees
-                 (id INTEGER PRIMARY KEY AUTOINCREMENT, name STRING, datetime STRING, department_id INTEGER, job_id INTEGER)''')
+    for row in content:
+        c.execute('INSERT INTO csv_data (name, age, city) VALUES (?, ?, ?)', row)
     conn.commit()
     conn.close()
-
-
-#jobs
-def create_table_jobs():
-    conn = sqlite3.connect('data.db')
-    c = conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS jobs
-                 (id INTEGER PRIMARY KEY AUTOINCREMENT, job STRING)''')
-    conn.commit()
-    conn.close()
-
-#departments
-def insert_batch_departments(transactions):
-    conn = sqlite3.connect('data.db')
-    c = conn.cursor()
-    c.executemany('INSERT INTO departments (STRING) VALUES (?)', transactions)
-    conn.commit()
-    conn.close()
-
 
 @app.route('/upload', methods=['POST'])
 def upload_csv():
@@ -53,20 +33,13 @@ def upload_csv():
         return jsonify({'error': 'No selected file'})
 
     if file:
-        # Save the file
-        file.save(file.filename)
-        # Read the CSV file and insert data into the database
-        with open(file.filename, 'r') as csv_file:
-            csv_reader = csv.reader(csv_file)
-            next(csv_reader)  # Skip the header row
-            transactions = [(row[0], int(row[1])) for row in csv_reader]
-            insert_batch_departments(transactions)
+        # Read the CSV file and insert each row into the database
+        csv_file = TextIOWrapper(file.stream, encoding='utf-8')
+        csv_reader = csv.reader(csv_file)
+        rows = list(csv_reader)
+        insert_csv_data(rows)
         return jsonify({'message': 'File uploaded successfully'})
 
-
 if __name__ == '__main__':
-    clean_departments()
-    create_table_departments()
-    create_table_hired_employees()
-    create_table_jobs()
-    app.run(debug=True) 
+    create_table()
+    app.run(debug=True) # You can set debug=False for production
--- .venv/cleaning_csv.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-#clean departments csv
-def clean_departments():
-    data= pd.read_csv("departments.csv",header=None,skiprows=0)
-    clean = data[0].str.split(",", n=1, expand=False)
-    clean.to_csv('departments.csv',index=False)
