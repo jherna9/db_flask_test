870a52d414e8336a74d0ec121b9ee0912d086e07
jherna9 <jherna1990@gmail.com>
2024-02-27 17:36:41 -0500

    department working

--- .venv/application.py
+++ .venv/application.py
@@ -1,29 +1,35 @@
 from flask import Flask, request, jsonify
 import sqlite3
 import csv
+import pandas as pd
 from io import TextIOWrapper
 
 app = Flask(__name__)
 
-# Function to create SQLite database table if not exists
-def create_table():
+# departments
+def clean_departments():
+    data= pd.read_csv("data/departments.csv",header=None,skiprows=0,names=['id','departments'])
+    data1 = data[['departments']]
+    data1.to_csv('data/departments_out.csv',index=False)
+
+def create_table_departments():
     conn = sqlite3.connect('data.db')
     c = conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS csv_data
-                 (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT,age INTEGER, city TEXT)''')
+    c.execute('''CREATE TABLE IF NOT EXISTS departments
+                 (id INTEGER PRIMARY KEY AUTOINCREMENT, department STRING)''')
     conn.commit()
     conn.close()
 
 # Function to insert CSV data into the database
-def insert_csv_data(content):
+def insert_departments(content):
     conn = sqlite3.connect('data.db')
     c = conn.cursor()
     for row in content:
-        c.execute('INSERT INTO csv_data (name, age, city) VALUES (?, ?, ?)', row)
+        c.execute('INSERT INTO departments (department) VALUES (?)', row)
     conn.commit()
     conn.close()
 
-@app.route('/upload', methods=['POST'])
+@app.route('/upload_departments',endpoint='func1', methods=['POST'])
 def upload_csv():
     if 'file' not in request.files:
         return jsonify({'error': 'No file part'})
@@ -37,9 +43,51 @@ def upload_csv():
         csv_file = TextIOWrapper(file.stream, encoding='utf-8')
         csv_reader = csv.reader(csv_file)
         rows = list(csv_reader)
-        insert_csv_data(rows)
+        insert_departments(rows)
+        return jsonify({'message': 'File uploaded successfully'})
+
+#hired_employees
+def clean_departments():
+    data= pd.read_csv("data/hired_employees.csv",header=None,skiprows=0,names=['id','name','datetime','department_id','job_id'])
+    data2 = data[['name','datetime','department_id','job_id']]
+    data2.to_csv('data/hired_employees.csv',index=False)
+
+def create_hired_employees():
+    conn = sqlite3.connect('data.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS hired_employees
+                 (id INTEGER PRIMARY KEY AUTOINCREMENT, name STRING, datetime STRING, department_id INTEGER, job_id INTEGER)''')
+    conn.commit()
+    conn.close()
+
+# Function to insert CSV data into the database
+def insert_hired_employees(content):
+    conn = sqlite3.connect('data.db')
+    c = conn.cursor()
+    for row in content:
+        c.executemany('INSERT INTO hired_employees (STRING,STRING,STRING,INTEGER,INTEGER) VALUES (?,?,?,?,?)', row)
+    conn.commit()
+    conn.close()
+
+@app.route('/upload_hired_employees',endpoint='func2', methods=['POST'])
+def upload_csv():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'})
+
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'})
+
+    if file:
+        # Read the CSV file and insert each row into the database
+        csv_file = TextIOWrapper(file.stream, encoding='utf-8')
+        csv_reader = csv.reader(csv_file)
+        rows = list(csv_reader)
+        insert_hired_employees(rows)
         return jsonify({'message': 'File uploaded successfully'})
 
 if __name__ == '__main__':
-    create_table()
+    clean_departments()
+    create_table_departments()
+    create_hired_employees()
     app.run(debug=True) # You can set debug=False for production
